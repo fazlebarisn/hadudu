c473a237b00c50eff34191764cf1387b37f20420
Fazle Bari <fazlebarisn@gmail.com>
2022-02-27 13:01:19 +0600

    replace FBS_THEME to HADUDU_THEME

--- inc/classes/class-assets.php
+++ inc/classes/class-assets.php
@@ -2,12 +2,12 @@
 /**
  * Bootstraps the Theme.
  *
- * @package Fbs
+ * @package Hadudu
  */
 
-namespace FBS_THEME\Inc;
+namespace HADUDU_THEME\Inc;
 
-use FBS_THEME\Inc\Traits\Singleton;
+use HADUDU_THEME\Inc\Traits\Singleton;
 
 class Assets {
 
@@ -27,8 +27,8 @@ class Assets {
 
 	public function registerStyles(){
 		// Register Syle
-		wp_register_style('stylesheet', get_stylesheet_uri(), [], filemtime( FBS_DIR_PATH . '/style.css'), 'all');
-		wp_register_style('bootstrap-css', FBS_DIR_URI . '/assets/src/library/css/bootstrap.min.css', [], false, 'all');
+		wp_register_style('stylesheet', get_stylesheet_uri(), [], filemtime( HADUDU_DIR_PATH . '/style.css'), 'all');
+		wp_register_style('bootstrap-css', HADUDU_DIR_URI . '/assets/src/library/css/bootstrap.min.css', [], false, 'all');
 
 		// Enqueue Style
 		wp_enqueue_style('bootstrap-css');
@@ -37,8 +37,8 @@ class Assets {
 
 	public function registerScripts(){
 		// Register Scripts
-		wp_register_script( 'main-js', FBS_DIR_URI . '/assets/main.js', [], filemtime( FBS_DIR_PATH . '/assets/main.js'), true );
-		wp_register_script( 'bootstrap-js', FBS_DIR_URI . '/assets/src/library/js/bootstrap.min.js', ['jquery'], false, true );
+		wp_register_script( 'main-js', HADUDU_DIR_URI . '/assets/main.js', [], filemtime( HADUDU_DIR_PATH . '/assets/main.js'), true );
+		wp_register_script( 'bootstrap-js', HADUDU_DIR_URI . '/assets/src/library/js/bootstrap.min.js', ['jquery'], false, true );
 
 		// Enqueue Script
 		wp_enqueue_script('bootstrap-js');
