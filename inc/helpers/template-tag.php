ef656332af294c5fc9e4b93764f1ce35de3f634c
Fazle Bari <fazlebarisn@gmail.com>
2023-08-02 23:39:18 +0600

    post date

--- inc/helpers/template-tag.php
+++ inc/helpers/template-tag.php
@@ -1,5 +1,8 @@
 <?php
 
+/**
+ * get post thumbnail
+ */
 function get_the_post_custom_thumbnail( $post_id, $size = 'featured-thumbnail', $additional_attributes = [] ){
 
     $custom_thumbnail = '';
@@ -27,6 +30,33 @@ function get_the_post_custom_thumbnail( $post_id, $size = 'featured-thumbnail', 
     return $custom_thumbnail;
 }
 
+/**
+ * Display the thumbnail
+ */
 function the_post_custom_thumbnail( $post_id, $size = 'featured-thumbnail', $additional_attributes = [] ){
     echo get_the_post_custom_thumbnail( $post_id, $size, $additional_attributes );
+}
+
+function hadudu_post_on(){
+    $time_string = '<time class="entry-date publish updated" datetime="%1$s">%2$s</time>';
+
+    // If the post is modified
+    if( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ){
+        $time_string = '<time class="entry-date publish" datetime="%1$s">%2$s</time><time class="updated" datetime="%3$s">%4$s</time>';
+    }
+
+    $time_string = sprintf(
+        $time_string,
+        esc_attr( get_the_date(DATE_W3C) ),
+        esc_attr( get_the_date() ),
+        esc_attr( get_the_modified_date(DATE_W3C) ),
+        esc_attr( get_the_modified_date() )
+    );
+
+    $post_on = sprintf(
+        esc_html_x( 'Post On %s', 'post date', 'hadudu'),
+        '<a href="'.esc_url(get_the_permalink()).'" rel="bookmark">' .$time_string.'</a>'
+    );
+
+    echo '<span class="posted-on text-secondary">'. $post_on.'</span>';
 }
