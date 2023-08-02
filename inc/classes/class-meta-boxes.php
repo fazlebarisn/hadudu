7bcb9c6d22f0483f6a57fba0ff38ac03b717a43c
Fazle Bari <fazlebarisn@gmail.com>
2023-08-02 21:44:55 +0600

    Save post metadata

--- inc/classes/class-meta-boxes.php
+++ inc/classes/class-meta-boxes.php
@@ -21,6 +21,7 @@ class Meta_Boxes {
 	protected function setup_hooks() {
 		// actions hooks
 		add_action( 'add_meta_boxes' , [ $this, 'hide_title_metabox' ] );
+        add_action( 'save_post' , [ $this, 'save_post_metadata' ] );
 	}
 
     /**
@@ -33,7 +34,7 @@ class Meta_Boxes {
         foreach( $screens as $screen ){
             add_meta_box(
                 'hide_page_title',
-                __('Hide The Page Title', 'hadudu'),
+                __('Page Title Options', 'hadudu'),
                 [$this, 'hide_title_metabox_html'], // callback function
                 $screen,
                 'side' // display in sidebar
@@ -48,18 +49,36 @@ class Meta_Boxes {
      */
     public function hide_title_metabox_html( $post ){
         // Get value from database
-        $value = get_post_meta( $post->ID,'hide_page_title',true );
+        $value = get_post_meta( $post->ID,'_hide_page_title',true );
 
         ?>
-        <label for="hadudu_title_field"><?php esc_html_e('Hide Page Title', 'hadudu') ?></label>
-        <select name="hadudu_title_field" id="hadudu_title_field" class="postbox">
-            <option value=""><?php esc_html_e('Select Option', 'hadudu') ?></option>
-            <option value="yes" <?php selected( $value, 'yes' ); ?>><?php esc_html_e('Show', 'hadudu') ?></option>
-            <option value="no" <?php selected( $value, 'no' ); ?>><?php esc_html_e('Hide', 'hadudu') ?></option>
+        <label for="hadudu-title-field"><?php esc_html_e('Hide Page Title', 'hadudu') ?></label>
+        <select name="hadudu_hide_title_field" id="hadudu-title-field" class="postbox">
+            <option value="">
+                <?php esc_html_e('Select Option', 'hadudu') ?>
+            </option>
+            <option value="yes" <?php selected( $value, 'yes' ); ?>>
+                <?php esc_html_e('Show', 'hadudu') ?>
+            </option>
+            <option value="no" <?php selected( $value, 'no' ); ?>>
+                <?php esc_html_e('Hide', 'hadudu') ?>
+            </option>
         </select>
         <?php
-
     }
     
+    /**
+     * Here we will save the post metadata
+     * @param $post_id
+     */
+    public function save_post_metadata( $post_id ){
+        if( array_key_exists( 'hadudu_hide_title_field', $_POST )){
+            update_post_meta(
+                $post_id,
+                '_hide_page_title',
+                $_POST['hadudu_hide_title_field'],
+            );
+        }
+    }
 
 }
