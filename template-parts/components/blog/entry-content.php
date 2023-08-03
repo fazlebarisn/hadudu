4e73751d3af81103bf415f48909e472fa4c82ce6
Fazle Bari <fazlebarisn@gmail.com>
2023-08-03 16:45:55 +0600

    hadudu_the_excerpt function added

---
+++ template-parts/components/blog/entry-content.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Template for entry content
+ * @package hadudu
+ */
+?>
+<div class="entry-content">
+    <?php
+    if( is_single() ){
+        the_content(
+            sprintf(
+                wp_kses(
+                    __('Read more %s <span class="meta-nav">&rarr;</span>', 'hadudu'),
+                    [
+                        'span' => [
+                            'class' => []
+                        ]
+                    ]
+                    ),
+                    the_title('<span class="screen-reader-text">"', '"</span>', false )
+            )
+        );
+    }else{
+        hadudu_the_excerpt();
+    }
+    ?>
+</div>
