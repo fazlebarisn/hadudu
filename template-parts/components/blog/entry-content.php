ef78e52572ff82cefbc23c45e4382b184e15ff4a
Fazle Bari <fazlebarisn@gmail.com>
2023-08-06 16:31:57 +0600

    Single page pagination done

--- template-parts/components/blog/entry-content.php
+++ template-parts/components/blog/entry-content.php
@@ -20,9 +20,22 @@
                     the_title('<span class="screen-reader-text">"', '"</span>', false )
             )
         );
+
+        /**
+         * Single post pagination
+         * If page_break use inside of a post
+         */
+        wp_link_pages(
+            [
+                'before' => '<div class="page-links">' . esc_html( 'Pages', 'hadudu'),
+                'after' =>  '</div>'
+            ]
+        );
+
     }else{
         hadudu_the_excerpt();
         echo hadudu_excerpt_more();
     }
+
     ?>
 </div>
