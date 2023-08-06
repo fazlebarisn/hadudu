70a8069f1a7c64f55b0a66a026c52a89b421ad1d
Fazle Bari <fazlebarisn@gmail.com>
2023-08-06 16:14:54 +0600

    single post done

--- single.php
+++ single.php
@@ -1,12 +1,39 @@
 <?php
-/*
+/** 
 * Single post template file
-* @packege fbs
+* @package Hadudu
 */
 get_header();
 ?>
-<div class="content">
-    <h1>Single post page</h1>
+<div id="primary">
+    <main id="main" class="site-main mt-5" role="main">
+        <?php
+            if( have_posts() ) :
+                ?>
+                    <div class="container">
+                        <?php
+                            if( is_home() && ! is_front_page() ){
+                                ?>
+                                    <header class="mb-5">
+                                        <h1 class="page-title">
+                                            <?php single_post_title(); ?>
+                                        </h1>
+                                    </header>
+                                <?php
+                            }
+                            // Start the loop
+                            while( have_posts() ) : the_post();
+                                get_template_part( 'template-parts/content' );
+                            endwhile;
+                            ?>
+                    </div>
+                <?php
+
+                else : 
+                    get_template_part( 'template-parts/content-none' );
+            endif;
+        ?>
+    </main>
 </div>
 
 <?php 
