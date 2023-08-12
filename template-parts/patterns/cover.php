b988ed5750714a663b1ca18e97713cd54f35ba08
Fazle Bari <fazlebarisn@gmail.com>
2023-08-12 13:06:43 +0600

    template conent

---
+++ template-parts/patterns/cover.php
@@ -0,0 +1,20 @@
+<?php
+/** 
+* Cover patterns content
+* @package Hadudu
+*/
+?>
+
+<div class="wp-block-cover alignfull">
+    <span aria-hidden="true" class="wp-block-cover__background has-background-dim"></span>
+    <img class="wp-block-cover__image-background wp-image-1691" alt="" src="http://localhost/r/wpr/wp-content/uploads/2014/01/dsc20050315_145007_132.jpg" data-object-fit="cover"/>
+    <div class="wp-block-cover__inner-container">
+		<p class="has-text-align-center has-large-font-size">Make Your Own Way</p>
+		<p class="has-text-align-center">How to know yourself!</p>
+	    <div class="wp-block-buttons">
+			<div class="wp-block-button is-style-outline">
+                <a class="wp-block-button__link wp-element-button">Blog</a>
+            </div>
+        </div>
+    </div>
+</div>
