e729e6df9ea48eb1f3b45f69ef7faebb22de2318
Fazle Bari <fazlebarisn@gmail.com>
2023-08-12 20:49:47 +0600

    update

--- front-page.php
+++ front-page.php
@@ -1,14 +1,38 @@
 <?php
-/*
-* Front page template file
-* @packege fbs
-*/
+/**
+ * Front page template
+ *
+ * @package hadudu
+ */
 get_header();
+
 ?>
-<div class="content">
-    <h1>Front page</h1>
-</div>
 
-<?php 
+	<div id="primary">
+		<main id="main" class="site-main mt-5" role="main">
+			<div class="home-page-wrap">
+				<?php
+				if ( have_posts() ) :
+					while ( have_posts() ) : the_post();
 
+						get_template_part( 'template-parts/content', 'page' );
+
+					endwhile;
+					?>
+
+				<?php
+
+				else :
+
+					get_template_part( 'template-parts/content-none' );
+
+				endif;
+				// get_template_part( 'template-parts/components/posts-carousel' );
+				?>
+			</div>
+		</main>
+	</div>
+
+<?php
 get_footer();
+
--- page.php
+++ page.php
@@ -5,8 +5,22 @@
 */
 get_header();
 ?>
-<div class="content">
-    <h1>Default page</h1>
+<div id="primary">
+    <main id="main" class="site-main mt-5" role="main">
+        <div class="container">
+                <?php
+                if( have_posts() ) :
+                    // Start the loop
+                    while( have_posts() ) : the_post();
+                        get_template_part( 'template-parts/content', 'page' );
+                    endwhile;
+                    else : 
+                        get_template_part( 'template-parts/content-none' );
+                endif;
+                ?>
+        </div>
+        </div>
+    </main>
 </div>
 
 <?php 
