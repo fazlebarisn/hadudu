8ff426718cdc647d3d1aac30ecf488817c3c4691
Fazle Bari <fazlebarisn@gmail.com>
2023-08-12 11:35:52 +0600

    register pattern

--- inc/classes/class-block-patterns.php
+++ inc/classes/class-block-patterns.php
@@ -29,6 +29,21 @@ class Block_Patterns{
 				[
 					'title'	=> __('Hadudu Cover', 'hadudu'),
 					'description'	=> __('A Hadudu block with text and image', 'hadudu'),
+					'content'	=> '<!-- wp:cover {"url":"http://localhost/r/wpr/wp-content/uploads/2014/01/dsc20050315_145007_132.jpg","id":1691,"dimRatio":50,"align":"full"} -->
+					<div class="wp-block-cover alignfull"><span aria-hidden="true" class="wp-block-cover__background has-background-dim"></span><img class="wp-block-cover__image-background wp-image-1691" alt="" src="http://localhost/r/wpr/wp-content/uploads/2014/01/dsc20050315_145007_132.jpg" data-object-fit="cover"/><div class="wp-block-cover__inner-container"><!-- wp:paragraph {"align":"center","placeholder":"Write title…","fontSize":"large"} -->
+					<p class="has-text-align-center has-large-font-size">Make Your Own Way</p>
+					<!-- /wp:paragraph -->
+					
+					<!-- wp:paragraph {"align":"center"} -->
+					<p class="has-text-align-center">How to know yourself!</p>
+					<!-- /wp:paragraph -->
+					
+					<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"}} -->
+					<div class="wp-block-buttons"><!-- wp:button {"className":"is-style-outline"} -->
+					<div class="wp-block-button is-style-outline"><a class="wp-block-button__link wp-element-button">Blog</a></div>
+					<!-- /wp:button --></div>
+					<!-- /wp:buttons --></div></div>
+					<!-- /wp:cover -->'
 				]
 			);
 		}
